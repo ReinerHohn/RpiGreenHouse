9f1bebede195062f4dea36b582b35dda67f335b8
Michael Dick <mi-dick@gmx.de>
2016-07-29 19:59:28 +0200

    Cap Sensor lesen geht!

---
+++ server/cc3200CapSens.c
@@ -0,0 +1,41 @@
+#include "spiComm.h"
+
+#include <stdio.h>
+#include <time.h>
+
+static int fd;
+
+void initcc3200CapSens()
+{
+    fd = openSpi("/dev/spidev0.0", 100000, 0, 8, 0);
+}
+
+void exitcc3200CapSens()
+{
+    closeSpi(fd);
+}
+
+int getHumValue()
+{
+    uint8_t   auTxData[4] = {0x01, 0, 0, 0, 0};
+    uint8_t   auRxData[4] = {0, };
+    int       nHumVal;
+
+    nHumVal = 0;
+
+    auTxData[0] = 1;
+    auTxData[1] = 1;
+    auTxData[2] = 1;
+    auTxData[3] = 1;
+
+    while(1)
+    {
+        transfer(fd, auTxData, ARRAY_SIZE(auTxData), auRxData, 100000, 0, 8);
+        long ulFreq = (auRxData[0] << 24) | (auRxData[1] << 16) | (auRxData[2] << 8) | auRxData[3];
+        fprintf(stderr, "Frequency : %d Hz\n\n\r", ulFreq);
+        usleep(200000);
+    }
+    transfer(fd, auTxData, ARRAY_SIZE(auTxData), auRxData, 100000, 0, 8);
+    transfer(fd, auTxData, ARRAY_SIZE(auTxData), auRxData, 100000, 0, 8);
+    return nHumVal;
+}
---
+++ server/cc3200CapSens.h
@@ -0,0 +1,9 @@
+#ifndef CC3200_CAP_SENS
+#define CC3200_CAP_SENS
+
+
+void initcc3200CapSens();
+void exitcc3200CapSens();
+int getHumValue();
+
+#endif //CC3200_CAP_SENS
